8e53e2ce087ebb655f072ebefbe12b76fac7c82a
wililm <83148022+wililm@users.noreply.github.com>
2021-08-11 00:27:20 -0400

    Update blackjack.py
    The Deck Class

--- blackjack.py
+++ blackjack.py
@@ -7,3 +7,17 @@ class Card:
     
   def __repr__(self):
     return "of".join((self.value, self.suit))
+
+  
+class Deck:
+  def __init__(self):
+    self.cards = [Card(s, v) for s in ["Spades", "Clubs", "Hearts", "Diamonds"] for v in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]]
+        
+  def shuffle(self):
+    if len(self.cards) > 1:
+      random.shuffle(self.cards)
+      
+  def deal(self):
+    if len(self.cards) >1:
+      return self.cards.pop(0)
+      
