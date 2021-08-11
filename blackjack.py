d0ce3c6a83e9159ff947ff6a1367edd90de6a5c2
wililm <83148022+wililm@users.noreply.github.com>
2021-08-11 01:07:34 -0400

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,18 +1,54 @@
 import random
 
+
+SUITS = ("Hearts", "Diamonds", "Spades", "Clubs")
+RANKS = (
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten",
+    "Jack",
+    "Queen",
+    "King",
+    "Ace",
+)
+VALUES = {
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    "Five": 5,
+    "Six": 6,
+    "Seven": 7,
+    "Eight": 8,
+    "Nine": 9,
+    "Ten": 10,
+    "Jack": 10,
+    "Queen": 10,
+    "King": 10,
+    "Ace": 11,
+}
+
 class Card:
-    def __init__(self, suit, value):
+    def __init__(self, suit, rank):
         self.suit = suit
-        self.value = value
+        self.rank = rank
 
     def __repr__(self):
-        return " of ".join((self.value, self.suit))
+        return " of ".join((self.rank, self.suit))
   
 class Deck:
     def __init__(self):
-        self.cards = [Card(s, v) for s in ["Spades", "Clubs", "Hearts",
-                      "Diamonds"] for v in ["A", "2", "3", "4", "5", "6", 
-                      "7", "8", "9", "10", "J", "Q", "K"]]
+        self.deck = []
+        self.player = []
+        self.dealer = []
+        for suit in SUITS:
+            for rank in RANKS:
+                self.deck.append((suit, rank))
 
     def shuffle(self):
         if len(self.cards) > 1:
